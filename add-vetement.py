7b4b9039cbb6bcb8615cb6854e6f319d2a69df3c
wagigi <potdevin.silvain@gmail.com>
2018-11-29 00:49:53 +0100

    Add adding vetement post

---
+++ add-vetement.py
@@ -0,0 +1,37 @@
+import json
+import datetime
+import postgresql
+import postgresql.driver
+
+db = postgresql.driver.connect(
+    user='dress_up_user',
+    password='azerty123',
+    host='dress-up-d.cx6dq3mfz1fd.eu-west-1.rds.amazonaws.com',
+    database='dress_up',
+    port=5432
+)
+
+
+def handler(event, context):
+    data = json.loads(event['body'])
+
+    nom = data["nom"]
+    note_chaleur = data["note_chaleur"]
+    jour_max = data["jour_max"]
+    couleur = data["couleur"]
+    endroit = data["endroit"]
+
+    db.execute("""
+    INSERT INTO vetement (nom_vetement, note_chaleur, jourx_max_port, endroit_du_corps, couleur) 
+    VALUES ('{nom_vetement}',
+            {note_chaleur},
+            {jourx_max_port},
+            {endroit_du_corps},
+            {couleur}
+    );
+    """.format(nom_vetement=nom, note_chaleur=note_chaleur, jourx_max_port=jour_max, endroit_du_corps=endroit,
+               couleur=couleur))
+
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
