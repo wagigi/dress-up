d2e152d5d4f83481a6303e9aec5b0e52265b37ec
wagigi <potdevin.silvain@gmail.com>
2018-11-30 00:57:28 +0100

    Add choix vetement

--- choix-vetement.py
+++ choix-vetement.py
@@ -40,12 +40,22 @@ def handler(event, context):
     WHERE proprietaire = {id_user} and c.id_couleur = v.couleur and e.id = v.endroit_du_corps
     """)
 
-    user = db.prepare(f"""
-    select nom, prenom FROM "user"
+    ville = db.prepare(f"""
+    select ville FROM "user"
     WHERE id_user = {id_user}
     """)
 
-    reponse = requests.get(meteo_url + data['ville'])
+    la_ville = ville()
+    if not la_ville:
+        retour = {
+            'output': 'ko, ville not found',
+            'timestamp': datetime.datetime.utcnow().isoformat()
+        }
+        return {'statusCode': 400,
+                'body': json.dumps(retour),
+                'headers': {'Content-Type': 'application/json'}}
+
+    reponse = requests.get(meteo_url + la_ville[0][0])
 
     meteo = json.loads(reponse.content.decode('utf-8'))
 
@@ -57,36 +67,86 @@ def handler(event, context):
 
     temp_day_min = meteo["fcst_day_0"]["tmin"]
     temp_day_max = meteo["fcst_day_0"]["tmax"]
-    condition = meteo["fcst_day_0"]["condition_key"]
 
-    propri = user()
+    indice = decide_indice_chaleur_requis(temp_day_min, temp_day_max)
 
-    if not propri:
-        retour = {
-            'output': 'ko, user not found',
-            'timestamp': datetime.datetime.utcnow().isoformat()
-        }
-        return {'statusCode': 400,
-                'body': json.dumps(retour),
-                'headers': {'Content-Type': 'application/json'}}
+    vetements = db.prepare(f"""
+            select v.id_vetement, v.nom_vetement, c.couleur, v.note_chaleur, e.endroit, v.jourx_max_port, v.avantage
+            FROM vetement as v, couleur as c, endroit_corps as e
+            WHERE proprietaire = {id_user} and c.id_couleur = v.couleur and e.id = v.endroit_du_corps
+            """)
 
     les_vetements = vetements()
 
-    garde_robe = {}
+    if indice == 20:
+        chaleur_rech = 5
+    elif indice == 15:
+        chaleur_rech = 4
+    elif indice == 12:
+        chaleur_rech = 3
+    elif indice == 10:
+        chaleur_rech = 2
+    elif indice == 5:
+        chaleur_rech = 1
+    else:
+        chaleur_rech = 3
 
+    tenue = []
+    habits_choisi = []
     for vetement in les_vetements:
-        garde_robe[vetement[1]] = {"id": vetement[0],
-                                   "couleur": vetement[2],
-                                   "note chaleur": vetement[3],
-                                   "endroit": vetement[4],
-                                   "jours_max_port": vetement[5],
-                                   "avantage": vetement[6]
-                                   }
+        if vetement[4] not in habits_choisi:
+            if vetement[3] == chaleur_rech or vetement[3] == chaleur_rech - 1:
+                tenue.append(vetement)
+                habits_choisi.append(vetement[4])
 
-    retour = {"Nom": propri[0][0].capitalize(),
-              "Prenom": propri[0][1].capitalize(),
-              "garde_robe": garde_robe}
+    while 'jambes' not in habits_choisi:
+        indice_revu = 2
+        for vetement in les_vetements:
+            if vetement[4] not in habits_choisi:
+                if vetement[3] == chaleur_rech - indice_revu:
+                    tenue.append(vetement)
+                    habits_choisi.append(vetement[4])
+        indice_revu += 1
+
+    while 'torse' not in habits_choisi:
+        indice_revu = 2
+        for vetement in les_vetements:
+            if vetement[4] not in habits_choisi:
+                if vetement[3] == chaleur_rech - indice_revu:
+                    tenue.append(vetement)
+                    habits_choisi.append(vetement[4])
+        indice_revu += 1
+
+    retour = {"Tenue_du_jour": tenue}
 
     return {'statusCode': 200,
             'body': json.dumps(retour),
             'headers': {'Content-Type': 'application/json'}}
+
+
+def decide_indice_chaleur_requis(temp_min, temp_max):
+    if temp_max - temp_min < 5:
+        # use temp max only
+        if temp_max < 0:
+            return 20
+        elif temp_max < 10:
+            return 15
+        elif temp_max < 15:
+            return 12
+        elif temp_max < 20:
+            return 10
+        elif temp_max < 30:
+            return 5
+    else:
+        # use les deux temps
+        # TODO: Améliorer la décision ~
+        if temp_max < 0:
+            return 20
+        elif temp_max < 10:
+            return 15
+        elif temp_max < 15:
+            return 12
+        elif temp_max < 20:
+            return 10
+        elif temp_max < 30:
+            return 5
